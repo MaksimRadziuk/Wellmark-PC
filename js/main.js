42cea488cea937e40b359ef9146b4cd137d2c371
MaksimRadziuk <30619681+MaksimRadziuk@users.noreply.github.com>
2017-10-01 08:40:10 +0300

    01/10 8.40

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 //подключение timePicker
 $('input.time').timePicker();
 
+// работа label:checked B iOS & iPad
+var deviceAgent = navigator.userAgent.toLowerCase();
+    var iOS = deviceAgent.match(/(iphone|ipod|ipad)/);
+    if (iOS) {
+        $('label').click(function (event) {
+            $('#' + $(event.target).attr('for')).attr('checked', true).change();
+        });
+    }
