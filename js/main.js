5c2dd1f67494c2d26a47f96a28873021c747d1a9
MaksimRadziuk <30619681+MaksimRadziuk@users.noreply.github.com>
2017-10-03 05:49:21 +0300

    03/10 5.50

--- js/main.js
+++ js/main.js
@@ -11,24 +11,69 @@ var deviceAgent = navigator.userAgent.toLowerCase();
 
 
 //раздвигание блоков
+
+	//блок формы (+ смена бэкграунда action-right-block)
 var sl = document.getElementsByClassName("toggle-point");
 var i;
 
 for (i = 0; i < sl.length; i++) {
     sl[i].onclick = function() {
         this.classList.toggle("toggle-point");
-        this.nextElementSibling.classList.toggle("show");
-        this.nextElement.classList.toggle("show");
+        this.nextElementSibling.classList.toggle("hide");
+        this.lastElementChild.classList.toggle("checked");
     }
 }
 
-var sl = document.getElementsByClassName("delete");
+
+	//блок сайдбара
+var sl = document.getElementsByClassName("toggle1-point");
 var i;
 
 for (i = 0; i < sl.length; i++) {
     sl[i].onclick = function() {
-        this.classList.toggle("delete");
-        this.previousElementSibling.classList.toggle("hide");
-        this.previousElement.classList.toggle("hide");
+        this.classList.toggle("toggle1-point");
+        this.nextElementSibling.classList.toggle("show");
     }
 }
+
+// добавление блоков onclick(+ДОБАВИТЬ)
+function myFunc () {
+    document.getElementById("moscow").style.display = "inline-block";
+}
+function myFunc1 () {
+    document.getElementById("SpB").style.display = "inline-block";
+}
+function myFunc2 () {
+    document.getElementById("Novo").style.display = "inline-block";
+}
+function myShop1 () {
+    document.getElementById("shop1").style.display = "inline-block";
+}
+function myShop2 () {
+    document.getElementById("shop2").style.display = "inline-block";
+}
+function myAge1 () {
+    document.getElementById("over21").style.display = "inline-block";
+}
+function myAge2 () {
+    document.getElementById("over26").style.display = "inline-block";
+}
+function myAge3 () {
+    document.getElementById("over31").style.display = "inline-block";
+}
+
+// удаление блоков onclick(+ДОБАВИТЬ)
+
+$('.delete').on('click', function() {
+  $(this).parent('.shop-place').css('display', 'none');
+});
+
+// удаление строк в таблице GOODS
+
+$('.delete').on('click', function() {
+  $(this).closest('.table-row').css('display', 'none');
+});
+
+
+
+
